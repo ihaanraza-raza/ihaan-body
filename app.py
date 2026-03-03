1a51f154030836b1a85fe3a23dec4b3f0c7515d7
ihaanraza-raza <ihaanraza@gmail.com>
2026-03-03 11:41:38 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://example.com"
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+title = soup.title.string
+
+print("Website Title:", title)
